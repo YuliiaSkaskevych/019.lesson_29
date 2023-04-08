fa90171f1d68a92099d09d2bd7b0bb61cbe6e902
Yuliia Skaskevych <skaskevich2000@gmail.com>
2023-04-08 13:08:37 +0200

    hw_29

--- script.js
+++ script.js
@@ -9,14 +9,12 @@ class Flat {
     residents = new Array();
     addResident(resident){
         this.residents.push(resident);
-        console.log("Successfully added new resident!");
-        return `Array of residents: ${this.residents}`;
+        console.log(`Successfully added resident: ${resident.name}`);
     }
 }
 
 class House {
     flats = new Array();
-    maxCountFlats;
     constructor (maxCountFlats){
      this.maxCountFlats = maxCountFlats;
      }
@@ -24,29 +22,31 @@ class House {
     if (this.flats.length<this.maxCountFlats){
         this.flats.push(flat);
         console.log("Successfully added new flat!");
-        return `Count of flats: ${this.flats.length}`;
+        console.log(`Count of flats: ${this.flats.length}`);
+    } else {
+        console.log("Error! Count of flats is full!");
+        console.log(`Max count of flats: ${this.maxCountFlats}`);
     }
-    console.log("Error! Count of flats is full!");
-    return `Max count of flats: ${this.maxCountFlats}`;
     }
 }
 
 let person1 = new Person ("John", 'male');
 let person2 = new Person ("Maria", 'female');
 let person3 = new Person ("Alex", 'male');
+let person4 = new Person ("Nancy", 'female');
 
-let resident1 = person1.name;
-let resident2 = person2.name;
-let resident3 = person3.name;
 
 let flat1 = new Flat ();
 let flat2 = new Flat ();
+let flat3 = new Flat ();
 
-console.log(flat1.addResident(resident1));
-console.log(flat1.addResident(resident2));
-console.log(flat2.addResident(resident3));
+flat1.addResident(person1);
+flat1.addResident(person2);
+flat2.addResident(person3);
+flat3.addResident(person4);
 
 let house = new House (2);
 
-console.log(house.addFlat(flat1));
-console.log(house.addFlat(flat2));
+house.addFlat(flat1);
+house.addFlat(flat2);
+house.addFlat(flat3);
